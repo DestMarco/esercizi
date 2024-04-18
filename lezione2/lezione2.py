3f62b0a7656692a153af1626dc2c4d991cd09f45
DestMarco <marcolino1424@gmail.com>
2024-04-18 12:25:21 +0200

    risolto primo esercizio

---
+++ lezione2/lezione2.py
@@ -0,0 +1,13 @@
+#Marco Destefano
+#18/04/2024
+print("Hello word")
+"""
+Personal Message: Use a variable to represent a person’s 
+name, and print a message to that person. Your message should
+ be simple, such as, “Hello Eric, would you like to learn some Python today?”
+"""
+#questa variabbile contiene il nome 
+name:str="Mario"
+#questa variabbile contine il messagio
+message:str=f"Ciao{name}, ti va di imparare un pò di python insime? "
+print(message)
