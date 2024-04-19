e23527008f7101d01d2b45b507190f3f610bf828
DesTMarco <marcolino1424@gmail.com>
2024-04-19 12:57:41 +0200

    finito esercizio 2

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -6,3 +6,18 @@ def subtract (a:int,b:int):
     return z
 #richiamo la funzione 
 print(subtract(18,5))
+
+print("---------------------------------------------------------------------")
+
+
+def check_value(n:int):
+    if n==5:
+        print(f"{n} è uguale a 5")
+    elif n>5:
+        print(f"{n} è maggiore a 5")
+    elif n<5:
+        print(f"{n} è minore a 5")
+    
+
+check_value(4)
+
