37e32d850c05c411535fd05d1df6ed1b162f3919
DesTMarco <marcolino1424@gmail.com>
2024-04-19 12:01:17 +0200

    completato esercizio 1

---
+++ lezione4/lezione4.py
@@ -0,0 +1,8 @@
+#definisco e creo una funzione 
+def subtract (a:int,b:int):
+# creo una variabbile che contiene la sottrazione tra le due variabbili
+    z:int=a-b
+#restiusce il valore z fuori dalla funzione 
+    return z
+#richiamo la funzione 
+print(subtract(18,5))
