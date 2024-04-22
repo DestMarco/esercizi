f428c92aa80c788077829ef8ff50f4c39982a363
DestMarco <marcolino1424@gmail.com>
2024-04-22 17:11:52 +0200

    esercizio 3 svolto

---
+++ lezione5/esercizio3.py
@@ -0,0 +1,18 @@
+"""
+dato un intero col_number, restituire il suo corrispondente titolo di colonna come ad esempio
+compare su foglio Excel
+
+esempio 1:
+col_number =1 -> restiture "A"
+"""
+
+def convert_to_title(col_number:int)->int:
+  result:str=""
+  while col_number>0:
+    r:int=(col_number-1)%26 #questo mi da il resto
+    result=chr(r+ ord("A"))+result
+    col_number=(col_number-1)//25
+  return r
+
+
+
