3954efc3a717b9b420e98bdebb0ff47a73f7114e
DestMarco <marcolino1424@gmail.com>
2024-04-22 15:50:40 +0200

    ++

--- lezione5/lezione5.py
+++ lezione5/lezione5.py
@@ -1,11 +1,14 @@
 def is_palindromo(X):
     S:str=str(X)
     rev="".join(reversed(S))
-    
+    return S==rev
+"""  
     if S == rev:
       return True
     else:
        return False
+"""
+    
 
 print(is_palindromo(151))
 
