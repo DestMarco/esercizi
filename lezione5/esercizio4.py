33685aa3de36b6f3ae3e8e6b75386c319a31748d
DestMarco <marcolino1424@gmail.com>
2024-04-22 18:39:30 +0200

    esercizio 4 completato

---
+++ lezione5/esercizio4.py
@@ -0,0 +1,21 @@
+def majority_element(nums:list[int])->int:
+    #creo il contatore
+    # creo la variabbile del maggiore 
+    
+    #se il conteggio è zero, impostiamo il maggiore a num
+    for i in range(len(nums)):
+        # verifico se lelemento trovato comapare più di n/2 volte e se compagliono due elementi mi stapa none 
+        if nums.count(i)> len(nums)/2:
+            return i
+    return None 
+
+print(majority_element(nums=[3,3,3,4,4,4]))
+         
+
+
+
+
+        
+
+
+    
