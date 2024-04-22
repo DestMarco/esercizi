36a59aa488737c2c349aa5c55b71438e3b4fa5f8
DestMarco <marcolino1424@gmail.com>
2024-04-22 19:09:02 +0200

    compleato tuto

---
+++ lezione5/esercizio5.py
@@ -0,0 +1,15 @@
+"""
+data una lista di interi, spostare gli zerialla fine di 
+questa lista mantenendo l'ordine origginale 
+dei nueri che non sono zeri
+"""
+def move_zeroes(nums:list[int]):
+    zero_index=0
+    for i in nums:
+        if i !=0:
+            nums[zero_index]=i
+            zero_index +=1
+    for i in range(zero_index, len(nums)):
+        nums[i]=0
+    return nums 
+print(move_zeroes(nums=[1,0,3,0,4,0,8,0,10,0,0,0,0,0,0,1]))
