969c45492ec14b872c930de2603d666f272d5f34
DestMarco <marcolino1424@gmail.com>
2024-05-09 17:26:39 +0200

    ++

--- ripasso/ripasso.py
+++ ripasso/ripasso.py
@@ -15,17 +15,39 @@ print(persona1.calcola_eta())
 
 class Dipendente(person):
 
-    def __init__(self, name: str, username: str, data_di_nascita, genere) -> None:
+    def __init__(self, name: str, username: str, data_di_nascita:str, genere:str,ore_lavorate:str) -> None:
 
         super().__init__(name, username, data_di_nascita, genere)
+        self.ore_lavorate=ore_lavorate
+
     def calcola_stipendio(self)->float:
         return 500.0
 
-dipendente1=Dipendente(name="Marco",username="De Stefano",data_di_nascita="14/10/04",genere="Maschio")
+dipendente1=Dipendente(name="Marco",username="De Stefano",data_di_nascita="14/10/04",genere="Maschio",ore_lavorate=500)
 
 
+class professore(Dipendente):
+
+    def __init__(self, name: str,
+                  username: str,
+                  data_di_nascita: str,
+                  genere: str,
+                  ore_lavorate: int,
+                  materia_insegnata:str,
+                  ore_di_lezione:int) -> None:
+        
+        super().__init__(name, username, data_di_nascita, genere, ore_lavorate)
+        self.materia_insegnata:str=materia_insegnata
+        self.ore_di_lezione=ore_di_lezione
+
+professore1=professore(name="Marco",username="De Stefano",data_di_nascita="14/10/04",genere="Maschio",ore_lavorate=500,materia_insegnata="informatica",ore_di_lezione=100000)
 
 print(persona1.calcola_eta())
+
+print(dipendente1.ore_lavorate)
 print(dipendente1.name)
 print(dipendente1.calcola_eta())
 print(dipendente1.calcola_stipendio())
+
+print(professore1.materia_insegnata)
+print(professore1.ore_di_lezione)
