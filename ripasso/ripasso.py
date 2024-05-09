aeb47f9d55d3bdf0efbfc4115246d90a1e6f04d9
DestMarco <marcolino1424@gmail.com>
2024-05-09 15:58:20 +0200

    ++

--- ripasso/ripasso.py
+++ ripasso/ripasso.py
@@ -18,7 +18,7 @@ class Dipendente(person):
     def __init__(self, name: str, username: str, data_di_nascita, genere) -> None:
 
         super().__init__(name, username, data_di_nascita, genere)
-    def calcola_stipendio()->float:
+    def calcola_stipendio(self)->float:
         return 500.0
 
 dipendente1=Dipendente(name="Marco",username="De Stefano",data_di_nascita="14/10/04",genere="Maschio")
