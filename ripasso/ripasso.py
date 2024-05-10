f741aea0c7c777eac54f9fd1cdc59da380415bdc
DestMarco <marcolino1424@gmail.com>
2024-05-10 18:00:26 +0200

    ++

--- ripasso/ripasso.py
+++ ripasso/ripasso.py
@@ -1,21 +1,33 @@
 class person:
 
-    def __init__(self,name:str,username:str,data_di_nascita,genere) -> None:
-        self.name:str=name
+    def __init__(self,name:str,
+                 username:str,
+                 data_di_nascita:str,
+                 genere:str) -> None:
+        
+        self._name:str=name
         self.username:str=username
         self.data_di_nascita: str=data_di_nascita
         self.genere:str=genere
 
     def calcola_eta(self)->int:
         return 10
+    def __eq__(self, value: object) -> bool:
+      return value.get_name()=self._name
+
+
 
 persona1=person(name="Marco",username="De Stefano",data_di_nascita="14/10/04",genere="Maschio")
 
 print(persona1.calcola_eta())
 
+persona1._name # non fare 
 class Dipendente(person):
 
-    def __init__(self, name: str, username: str, data_di_nascita:str, genere:str,ore_lavorate:str) -> None:
+    def __init__(self, name: str,
+                  username: str, data_di_nascita:str,
+                    genere:str,
+                    ore_lavorate:str) -> None:
 
         super().__init__(name, username, data_di_nascita, genere)
         self.ore_lavorate=ore_lavorate
@@ -51,3 +63,6 @@ print(dipendente1.calcola_stipendio())
 
 print(professore1.materia_insegnata)
 print(professore1.ore_di_lezione)
+
+
+
