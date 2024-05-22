01a73295f73c5e97f039ddf8664156bd453aea95
DestMarco <marcolino1424@gmail.com>
2024-05-22 16:41:54 +0200

    ++

--- lezione10/lezione10.py
+++ lezione10/lezione10.py
@@ -1,3 +1,4 @@
+"""
 def is_symmetric(tree: list[int])-> bool:
     return are_mirrored(tree,1,2)
 
@@ -20,5 +21,26 @@ def are_mirrored(tree,left_index, right_index)-> bool:
 
     is_symmetric_external:bool=are_mirrored(tree,left_of_left,right_of_right)
     is_symmetric_internal:bool=are_mirrored(tree,right_of_left,left_of_right)
-
     return is_symmetric_internal and is_symmetric_external
+
+"""    
+import getpass
+
+
+print("esempio")
+username = input ("Enter Username")
+print("Username is:" + username)
+
+if username != "Marco":
+    print("Username non valido")
+else:
+    password = getpass.getpass("Enter Password: ")
+    print("Password entered")
+
+    if password != "Marcolino71":
+        print("Password non valida")
+    else:
+        print("Username e password validi")
+        
+
+
