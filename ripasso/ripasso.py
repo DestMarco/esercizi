d6d810b4addf5b86a0ec70a53600c7c6a8677323
DestMarco <marcolino1424@gmail.com>
2024-05-29 12:17:00 +0200

    ++

--- ripasso/ripasso.py
+++ ripasso/ripasso.py
@@ -1,3 +1,4 @@
+"""
 class person:
 
     def __init__(self,name:str,
@@ -63,6 +64,90 @@ print(dipendente1.calcola_stipendio())
 
 print(professore1.materia_insegnata)
 print(professore1.ore_di_lezione)
+"""
+"""
+from abc import ABC, abstractmethod
 
+class AbcAnimal(ABC):
+ 
+    @abstractmethod
+    def verso (self):
+        pass
+    @abstractmethod
+    def movimento(self):
+        pass
+
+
+
+class Serpente(AbcAnimal):
+    def __init__(self,nome) -> None:
+        super().__init__()
+        self.nome=nome
+        self.velocita:float=10000000000000.0
+    def verso(self):
+        return print("psssss")
+    def movimento(self, t):
+        return self.velocita * t
+       
+Serpente_1:Serpente=Serpente(nome="Jormungandr")
+Serpente_1.verso()
+Serpente_1.movimento(t=1000000000000)
+
+
+class gatto(AbcAnimal):
+    def __init__(self,nome) -> None:
+        super().__init__()
+        self.nome=nome
+        self.velocità=19.0
+    def verso(self):
+        return print("UwU")
+    def movimento(self, t):
+        return self.velocita * t
+gatto_1:gatto=gatto(nome="franco")
+gatto_1.verso()
+gatto_1.movimento(t=10)
+"""
+
+class ContoBancario:
+    total_account=0
+    def __init__(self,iban, saldo, nome) -> None:
+        self.iban=iban
+        self.nome=nome
+        self.saldo=saldo
+        
+        ContoBancario.total_account +=1
+    def deposito(self, importo):
+        self.importo=importo
+        print(f"{importo} depositato. il nuovo saldo è{self.saldo}")
+    def prelievo(self, importo):
+        self.saldo -=importo
+        print(f"{importo} prelevato. il nuovo saldo è {self.saldo}")
+    @classmethod
+    def get_total_accounts(cls):
+        print(f"Account totali creati: {cls.total_account}")
+    
+    @staticmethod
+    def valida_account(iban):
+        if isinstance (iban, int) and len (str(iban)) == 10:
+            print ("iban valido")
+            return True
+        else:
+            print("iban non valido")
+            return False
+        
+        
+account1=ContoBancario(1234567890,0,'Alice')
+account2=ContoBancario(9876543210,1000, 'Bob')
+
+account1.prelievo(500)
+account1.deposito(200)
+
+account2.prelievo(200)
+account2.deposito(100)
+
+ContoBancario.get_total_accounts()
+
+ContoBancario.valida_account(1234567890)
+ContoBancario.valida_account('12345ABCD')
 
 
