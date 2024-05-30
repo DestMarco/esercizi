fc50c8fb4d9093ab419a7212587c2fe36984213c
DestMarco <marcolino1424@gmail.com>
2024-05-30 12:51:00 +0200

    esercizi classe creata

---
+++ lezione11/lezione11.py
@@ -0,0 +1,75 @@
+"""
+Sviluppa un sistema in Python che gestisca le prenotazioni per un cinema. Il cinema ha diverse sale, 
+ognuna con un diverso film in programmazione. Gli utenti possono vedere quali film sono disponibili e 
+prenotare posti per un determinato spettacolo.
+
+ 
+Classi:
+- Film: Rappresenta un film con titolo e durata.
+ 
+- Sala: Rappresenta una sala con numero identificativo, film attualmente in programmazione, posti totali, 
+posti prenotati.
+Metodi:
+
+    - prenota_posti(num_posti): Prenota un certo numero di posti nella sala, se disponibili. Restituisce 
+        un messaggio di conferma o di errore.
+    - posti_disponibili(): Restituisce il numero di posti ancora disponibili nella sala.
+ 
+- Cinema: Gestisce le operazioni legate alla gestione delle sale.
+Metodi:
+
+    - aggiungi_sala(sala): Aggiunge una nuova sala al cinema.
+    - prenota_film(titolo_film, num_posti): Trova il film desiderato e tenta di prenotare posti. Restituisce un 
+        messaggio di stato.
+Test case:
+Un gestore del cinema configura le sale aggiungendo i film e i dettagli dei posti.
+Un cliente sceglie un film e prenota un certo numero di posti.
+Il sistema verifica la disponibilità e conferma o rifiuta la prenotazione.
+"""
+
+class Film:
+    def __init__(self, titolo, durata) :
+        self.titolo=titolo
+        self.durata=durata
+class Sala:
+    def __init__(self, numeri, film, posti_totali) :
+        self.numeri=numeri
+        self.film=film
+        self.posti_totali=posti_totali
+        self.posti_prenotati=0
+        
+    
+class Cinema:
+    def __init__(self):
+        self.sale=[]
+        
+    def aggiungi_sale(self,sala):
+        self.sale.append(sala)
+        
+    def prenota_film(self,titolo_film,num_posti):
+        for sala in self.sale:
+            if sala.film.titolo == titolo_film:
+                return f"Film{titolo_film} non trovato in nessuna sala" 
+            
+        
+    def prenota_posti(self, num_posti):
+        if self.posti_prenotati + num_posti <= self.posti_totali:
+            self.posti_prenotati += num_posti
+            return f"prenotazione effetuta per {num_posti} per la sala {self.numero} per il fil {self.film.titolo}"
+        else:
+            return "Posti non disponibbili per questa prenotazione"
+        
+    def posti_disponibbili(self):
+        return self.posti_totali -self.posti_prenotati
+    
+sala1 = Sala(1, Film("Il padrino", 180), 100)
+sala2 = Sala(2, Film("Interstellar", 169), 80)
+sala3 = Sala(3, Film("La La Land", 128), 120)
+
+Cinema.aggiungi_sale(sala1)
+Cinema.aggiungi_sale(sala2)
+Cinema.aggiungi_sale(sala3)
+
+print(Cinema.prenota_film("Interstellar", 2))  
+print(Cinema.prenota_film("Interstellar", 90))  
+print(Cinema.prenota_film("Inception", 2)) 
