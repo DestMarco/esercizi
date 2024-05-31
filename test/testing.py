0567fcb53d71c42b92c0b43d2be8f7861a1ada36
DestMarco <marcolino1424@gmail.com>
2024-05-31 13:52:14 +0200

    ++

---
+++ test/testing.py
@@ -0,0 +1,11 @@
+import unittest
+from ..ripasso.ripasso2 import Calc
+class TestCalculetion(unittest.TestCase):
+    def setUp(self):
+        self.calculetion=Calc(8,2)
+        
+    def test_sum(self):
+        self.assertEqual(self.calculetion.get_sum(),10,'The sum is wrong')
+        
+if __name__ =="__main__":
+    unittest.main()
